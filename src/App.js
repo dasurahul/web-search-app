68e7b037dff01507fa655bbd8e9dde589217d46e
dasurahul <dasurahul598@gmail.com>
2021-07-03 16:40:36 +0530

    Fifth Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Search from "./components/Search";
 import News from "./components/News";
 
 const App = () => {
   return (
     <div>
+      <Search />
       <News />
     </div>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+
+const Search = () => {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/spelling/AutoComplete",
+      params: { text: input },
+      headers: {
+        "x-rapidapi-key": "d84e7a8fb6msh1b306d85f61613ap102416jsn062c1ca70521",
+        "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setItems(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [input]);
+  const inputHandler = (event) => {
+    setInput(event.target.value);
+  };
+  return (
+    <Container>
+      <Form
+        style={{
+          width: "90%",
+          margin: "20px auto",
+          maxWidth: "500px",
+          position: "relative",
+        }}
+      >
+        <Form.Control
+          type="text"
+          onChange={inputHandler}
+          value={input}
+          placeholder="Search"
+        />
+        <ListGroup
+          style={{ position: "absolute", zIndex: "1", left: "0", right: "0" }}
+        >
+          {items.map((item) => {
+            return <ListGroup.Item key={item}>{item}</ListGroup.Item>;
+          })}
+        </ListGroup>
+      </Form>
+    </Container>
+  );
+};
+
+export default Search;
