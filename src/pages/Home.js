b56d6eb4835f3f22ca966a68ab895196988974b1
dasurahul <dasurahul598@gmail.com>
2021-07-05 10:41:24 +0530

    My Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
                 }}
               />
             }
-            label="See News"
+            label="Show News"
             labelPlacement="end"
           />
         </FormGroup>
